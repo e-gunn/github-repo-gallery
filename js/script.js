7d55c04151378475b44e2819dadd768f82c9b2ef
e-gunn <gunn816@hotmail.com>
2021-05-04 20:22:54 -0700

    Add list of user repos

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const overview = document.querySelector(".overview");
 const username = "e-gunn";
+const repoList = document.querySelector(".repo-list");
 
 // get information from github profile
 const getProfile = async function () {
@@ -10,6 +11,7 @@ const getProfile = async function () {
 };
 getProfile();
 
+// display infomation from github profile 
 const displayProfile = function (profile) {
     // create new div 
     let newDiv = document.createElement("div");
@@ -28,3 +30,25 @@ const displayProfile = function (profile) {
     `;
     overview.append(newDiv);
 };
+
+// get information from user's public repos 
+const getRepos = async function () {
+    const res = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page`);
+    const repos = await res.json();
+    // console.log(repos);
+    displayRepos(repos);
+};
+getRepos();
+
+// display information from each repo
+const displayRepos = function (repos) {
+    for (const repo of repos) {
+        // create list item for each repo
+        let listItem = document.createElement("li");
+        // give each list item a heading
+        listItem.classList.add("repo");
+        listItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(listItem);
+    }
+};
+
